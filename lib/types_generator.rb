44167d060a1137bc1646d2194711b4e9dcde2a7a
kmdtmyk <winback_banderas@yahoo.co.jp>
2025-04-03 22:23:59 +0900

    bundle gem init

---
+++ lib/types_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "types_generator/version"
+
+module TypesGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/types_generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TypesGenerator do
+  it "has a version number" do
+    expect(TypesGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
