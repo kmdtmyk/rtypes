f992770977d349a7e7c52bc9e95feafaf9ba9648
kmdtmyk <winback_banderas@yahoo.co.jp>
2025-04-04 05:58:28 +0900

    Update schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_03_165515) do
+ActiveRecord::Schema.define(version: 2025_04_03_165515) do
+
   create_table "posts", force: :cascade do |t|
     t.string "datetime"
     t.string "title"
     t.string "body"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
