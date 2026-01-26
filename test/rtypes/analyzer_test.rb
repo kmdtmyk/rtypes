457bf638c04ef98237ba2ae9378ac5c6c49dea8e
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-01-26 23:04:09 +0900

    Refactoring

--- test/rtypes/analyzer_test.rb
+++ test/rtypes/analyzer_test.rb
@@ -3,35 +3,31 @@ require "test_helper"
 class Rtypes::AnalyzerTest < ActiveSupport::TestCase
 
   test 'attributes' do
-    analyzer = Rtypes::Analyzer.new(PostSerializer)
     assert_equal [
       { type: :integer, name: 'id' },
       { type: :string, name: 'title' },
       { type: :string, name: 'body' },
-    ], analyzer.attributes
+    ], Rtypes::Analyzer.new(PostSerializer).attributes
 
-    analyzer = Rtypes::Analyzer.new(UserSerializer)
     assert_equal [
       { type: :integer, name: 'id' },
       { type: :string, name: 'name' },
       { type: :boolean, name: 'admin' },
       { type: nil, name: 'any', options: { typescript: 'any' } },
-    ], analyzer.attributes
+    ], Rtypes::Analyzer.new(UserSerializer).attributes
   end
 
   test 'associations' do
-    analyzer = Rtypes::Analyzer.new(PostSerializer)
     assert_equal [
       { type: :belongs_to, name: 'user', class_name: 'User', serializer: UserSerializer },
       { type: :belongs_to, name: 'delete_user', class_name: 'User', serializer: UserSerializer },
       { type: :has_many, name: 'comments', class_name: 'Comment', serializer: nil },
-    ], analyzer.associations
+    ], Rtypes::Analyzer.new(PostSerializer).associations
 
-    analyzer = Rtypes::Analyzer.new(UserSerializer)
     assert_equal [
       { type: :has_many, name: 'posts', class_name: 'Post', serializer: PostSerializer },
       { type: :has_one, name: 'latest_post', class_name: 'Post', serializer: PostSerializer },
-    ], analyzer.associations
+    ], Rtypes::Analyzer.new(UserSerializer).associations
   end
 
 end
