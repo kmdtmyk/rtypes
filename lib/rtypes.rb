75016cc58074979615a89589b2cf8466c5f6ac84
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-01-26 21:34:19 +0900

    Add file for typescript

--- lib/rtypes.rb
+++ lib/rtypes.rb
@@ -1,139 +1,20 @@
 require "rtypes/version"
 require "rtypes/railtie"
 require "rtypes/analyzer"
+require "rtypes/type_script"
 
 require 'listen'
 
 class Rtypes
 
   def initialize(serializer)
-    if serializer == nil
-      return
-    end
-
     @serializer = serializer
     @model = Rtypes.serializer_to_model(serializer)
   end
 
   def generate
-    if invalid?
-      return
-    end
-    FileUtils.mkdir_p(File.dirname(file_path))
-    File.open(file_path, 'w') do |f|
-      f.puts file_content
-      f
-    end
+    Rtypes::TypeScript.new(@serializer).generate
   end
-
-  def file_name
-    if invalid?
-      return
-    end
-
-    [
-      @serializer.to_s.deconstantize.underscore,
-      "#{@model.name}.ts",
-    ].compact_blank.join('/')
-  end
-
-  def file_path
-    Rails.root.join(Rtypes.config.path, file_name).to_s rescue nil
-  end
-
-  def file_content
-    if invalid?
-      return
-    end
-
-    properties = []
-
-    analyzer = Rtypes::Analyzer.new(@serializer)
-    analyzer.attributes.each do |attribute|
-
-      type = if attribute.dig(:options, :typescript).present?
-        attribute.dig(:options, :typescript)
-      elsif self.class.config.types.has_key?(attribute[:type])
-        self.class.config.types[attribute[:type]]
-      else
-        'string'
-      end
-
-      properties << "#{attribute[:name].camelize(:lower)}: #{type}"
-    end
-
-    associations = analyzer.associations
-
-    associations
-      .filter{ _1[:serializer].present? }
-      .group_by{ _1[:class_name] }.each do |class_name, associations|
-        serializers = associations.map{ _1[:serializer] }.uniq
-        if 1 < serializers.size
-          associations.each{ _1[:import_name] = "#{_1[:class_name]}#{serializers.index(_1[:serializer]) + 1}" }
-        else
-          associations.each{ _1[:import_name] = _1[:class_name] }
-        end
-      end
-
-    associations.each do |association|
-      association[:module_name] = module_name(association[:class_name], association[:serializer])
-    end
-
-    associations.each do |association|
-      type = if association[:serializer].present?
-        association[:import_name]
-      else
-        'any'
-      end
-
-      if association[:type] == :has_many
-        properties << "#{association[:name].camelize(:lower)}?: Array<#{type}>"
-      else
-        properties << "#{association[:name].camelize(:lower)}?: #{type}"
-      end
-    end
-
-    result = [
-      "type #{@model.name} = {",
-      *properties.map{ "  #{_1}" },
-      "}\n",
-      "export default #{@model.name}\n"
-    ]
-
-    imports = associations
-      .filter{ _1[:module_name].present? }
-      .map{ "import #{_1[:import_name]} from '#{_1[:module_name]}'"}
-      .uniq
-
-    if imports.present?
-      imports << ''
-    end
-
-    (imports + result).join("\n")
-  end
-
-  private
-
-    def serializer_depth(serializer)
-      serializer.to_s.split('::').size
-    end
-
-    def module_name(class_name, serializer)
-      if serializer.nil?
-        return
-      end
-      own_depth = serializer_depth(@serializer)
-      import_depth = serializer_depth(serializer)
-      if own_depth == import_depth
-        "./#{class_name}"
-      else
-        "#{'../' * (own_depth - import_depth)}#{class_name}"
-      end
-    end
-
-    def invalid?
-      @serializer == nil || @model == nil
-    end
 
   class << self
 
---
+++ test/rtypes/type_script_test.rb
@@ -0,0 +1,156 @@
+require "test_helper"
+
+class Rtypes::TypeScriptTest < ActiveSupport::TestCase
+
+  def setup
+    Rtypes.instance_variable_set(:@config, nil)
+  end
+
+  test 'file_name' do
+    rtypes = Rtypes::TypeScript.new(UserSerializer)
+    assert_equal 'User.ts', rtypes.file_name
+    rtypes = Rtypes::TypeScript.new(Namespace1::PostSerializer)
+    assert_equal 'namespace1/Post.ts', rtypes.file_name
+    rtypes = Rtypes::TypeScript.new(nil)
+    assert_nil rtypes.file_name
+  end
+
+  test 'file_path' do
+    skip if ENV['CI']
+    rtypes = Rtypes::TypeScript.new(UserSerializer)
+    assert_equal '/app/test/dummy/app/javascript/types/User.ts', rtypes.file_path
+    Rtypes.config.path = 'app/frontend/entrypoints/types'
+    assert_equal '/app/test/dummy/app/frontend/entrypoints/types/User.ts', rtypes.file_path
+    rtypes = Rtypes::TypeScript.new(nil)
+    assert_nil rtypes.file_path
+  end
+
+  test 'file_content has_many and has_one' do
+    rtypes = Rtypes::TypeScript.new(UserSerializer)
+    assert_equal <<~EOS, rtypes.file_content
+      import Post from './Post'
+
+      type User = {
+        id: number
+        name: string
+        admin: boolean
+        any: any
+        posts?: Array<Post>
+        latestPost?: Post
+      }
+
+      export default User
+    EOS
+  end
+
+  test 'file_content belongs_to' do
+    rtypes = Rtypes::TypeScript.new(PostSerializer)
+    assert_equal <<~EOS, rtypes.file_content
+      import User from './User'
+
+      type Post = {
+        id: number
+        title: string
+        body: string
+        user?: User
+        deleteUser?: User
+        comments?: Array<any>
+      }
+
+      export default Post
+    EOS
+  end
+
+  test 'file_content with namespace' do
+    rtypes = Rtypes::TypeScript.new(Namespace1::PostSerializer)
+    assert_equal <<~EOS, rtypes.file_content
+      import User from '../User'
+
+      type Post = {
+        id: number
+        title: string
+        user?: User
+      }
+
+      export default Post
+    EOS
+  end
+
+  test 'file_content with namespace2' do
+    rtypes = Rtypes::TypeScript.new(Namespace2::PostSerializer)
+    assert_equal <<~EOS, rtypes.file_content
+      import User1 from './User'
+      import User2 from '../User'
+
+      type Post = {
+        id: number
+        title: string
+        user?: User1
+        deleteUser?: User2
+      }
+
+      export default Post
+    EOS
+  end
+
+  test 'file_content non exist model' do
+    rtypes = Rtypes::TypeScript.new(NonExistModelSerializer)
+    assert_nil rtypes.file_content
+    assert_nil rtypes.generate
+  end
+
+  test 'file_content nil' do
+    rtypes = Rtypes::TypeScript.new(nil)
+    assert_nil rtypes.file_content
+    assert_nil rtypes.generate
+  end
+
+  test 'config' do
+    assert_equal 'app/javascript/types', Rtypes.config.path
+    Rtypes.config.path = 'app/frontend/entrypoints/types'
+    assert_equal 'app/frontend/entrypoints/types', Rtypes.config.path
+  end
+
+  test 'config_file_content' do
+    assert_equal <<~EOS, Rtypes.config_file_content
+      Rtypes.config.path = 'app/javascript/types'
+      Rtypes.config.types = {
+        integer: 'number',
+        decimal: 'string',
+        boolean: 'boolean',
+      }
+
+      if Rails.env.development?
+        Rtypes.auto_generate
+      end
+    EOS
+  end
+
+  test 'name_to_serializer' do
+    assert_equal UserSerializer, Rtypes.name_to_serializer('user')
+    assert_equal UserSerializer, Rtypes.name_to_serializer('User')
+    assert_equal UserSerializer, Rtypes.name_to_serializer('UserSerializer')
+    assert_equal Namespace2::UserSerializer, Rtypes.name_to_serializer('Namespace2::UserSerializer')
+    assert_nil Rtypes.name_to_serializer('foo')
+    assert_nil Rtypes.name_to_serializer(nil)
+  end
+
+  test 'path_to_serializer' do
+    assert_equal UserSerializer, Rtypes.path_to_serializer('/app/test/dummy/app/serializers/user_serializer.rb')
+    assert_equal Namespace2::UserSerializer, Rtypes.path_to_serializer('/app/test/dummy/app/serializers/namespace2/user_serializer.rb')
+    assert_nil Rtypes.path_to_serializer('/app/test/dummy/app/serializers/user_serializer copy.rb')
+    assert_nil Rtypes.path_to_serializer('/app/test/dummy/app/serializers/dummy_serializer.rb')
+    assert_nil Rtypes.path_to_serializer('/app/test/dummy/app/serializers/README.md')
+    assert_nil Rtypes.path_to_serializer(nil)
+  end
+
+  test 'path_to_delete_file_path' do
+    assert_equal '/app/test/dummy/app/javascript/types/User.ts', Rtypes.path_to_delete_file_path('/app/test/dummy/app/serializers/user_serializer.rb')
+    assert_equal '/app/test/dummy/app/javascript/types/namespace2/User.ts', Rtypes.path_to_delete_file_path('/app/test/dummy/app/serializers/namespace2/user_serializer.rb')
+    Rtypes.config.path = 'app/frontend/entrypoints/types'
+    assert_equal '/app/test/dummy/app/frontend/entrypoints/types/User.ts', Rtypes.path_to_delete_file_path('/app/test/dummy/app/serializers/user_serializer.rb')
+    assert_nil Rtypes.path_to_serializer('/app/test/dummy/app/serializers/README.md')
+    assert_nil Rtypes.path_to_delete_file_path(nil)
+  end
+
+end
