75bb458119680a560acbd513fa50daa4d1c693a8
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-02-06 01:40:10 +0900

    Add config to initializer file

--- lib/tasks/templates/initializer.rb
+++ lib/tasks/templates/initializer.rb
@@ -5,6 +5,7 @@ Rtypes.config.types = {
   boolean: 'boolean',
 }
 Rtypes.config.enable_kotlin = false
+Rtypes.config.kotlin_package_name = 'your.pacakge.name'
 
 if Rails.env.development?
   Rtypes.auto_generate
